598538c2c0a135b3b3d003296f8d08aedc534a38
Teodor Pomohaci <97389197+VineBoomSoundEffect@users.noreply.github.com>
2026-02-27 22:39:26 +0200

    Create funcs.h

---
+++ lab2/ex2/funcs.h
@@ -0,0 +1,7 @@
+#pragma once
+
+int compareName(Student s1, Student s2);
+int compareMath(Student s1, Student s2);
+int compareEng(Student s1, Student s2);
+int compareHist(Student s1, Student s2);
+int compareAvg(Student s1, Student s2);
